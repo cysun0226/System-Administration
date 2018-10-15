9a33c2c18e69839a47a73365eeed6c551ecffcf4
cysun0226 <j6256448@gmail.com>
2018-10-15 21:12:38 +0800

    add argv

--- hw2/parse_json.cpp
+++ hw2/parse_json.cpp
@@ -12,8 +12,10 @@
 // [ and ] => 'START_ARRAY' and 'END_ARRAY'
 #define START_ARRAY '['
 #define END_ARRAY ']'
+#define CLASSES_FILE "classes.txt"
 
 char line[SIZE];
+std::fstream f_out;
 
 using namespace std;
 
@@ -26,32 +28,40 @@ void get_value(string str, std::size_t pos) {
       if (semi_colon_count == 2)
         continue;
       if (semi_colon_count == 3){
-        cout << endl;
+        f_out << "  ";
+        //cout << endl;
         break;
       }
     }
     if (semi_colon_count == 2)
-      cout << str[pos];
+      f_out << str[pos];
+      //cout << str[pos];
   }
 }
 
 void parse_json(string str){
-  string token[] = { "cos_id", "cos_time", "cos_ename"};
-
-  // course id
-  for (int i = 0; i < 3; i++) {
+  string token[] = { /*"cos_id",*/ "cos_time", "cos_ename"};
+  for (int i = 0; i < 2; i++) {
     std::size_t pos = str.find(token[i]);
     if(pos < str.length()) {
       get_value(str, pos);
+      if(i == 1)
+        f_out << endl;
     }
   }
 }
 
-int main() {
+int main(int argc, char *argv[]) {
   fstream fin;
-    fin.open("timetable.json",ios::in);
+	f_out.open(argv[2], ios::out);
+	if(!f_out)
+		cout<<"Fail to open file: "<<CLASSES_FILE<<endl;
+
+    fin.open(argv[1],ios::in);
     while(fin.getline(line,sizeof(line),'\n')){
         parse_json(line);
     }
+
+  f_out.close();
   return 0;
 }
