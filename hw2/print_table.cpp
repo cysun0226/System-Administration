8c6162e45e8d8354532370f9cd146d5a066b7a7e
cysun0226 <j6256448@gmail.com>
2018-10-17 02:17:31 +0800

    finish print timetable

--- hw2/print_table.cpp
+++ hw2/print_table.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 std::string weekdays[] = { "Mon", "Tue", "Wed", "Thu", "Fri" };
 std::string time_code[] = { "A", "B", "C", "D", "E", "F", "G", "H","I", "J", "K" };
-std::string timetable[11][5];
+std::string timetable[11][5][4];
 char line[SIZE];
 
 void print_bar() {
@@ -26,7 +26,7 @@ void print_dbar() {
 }
 
 int time_to_int(char t){
-  return t - 65;
+  return t - '1';
 }
 
 int day_to_int(string d){
@@ -60,20 +60,14 @@ void print_table() {
      // time_row
      cout << " " << time_code[t] << " ";
      for (int w = 0; w < 5; w++) {
-       cout << "|" << "x.";
-       for (int i = 0; i < 15; i++) {
-         cout << " ";
-       }
+       cout << "| " << timetable[t][w][0] << " ";
      }
      cout << "|" << endl;
      // grid
-     for (int g = 0; g < 3; g++) {
+     for (int g = 1; g < 4; g++) {
        cout << "   ";
        for (int w = 0; w < 5; w++) {
-         cout << "|" << ".";
-         for (int i = 0; i < 16; i++) {
-           cout << " ";
-         }
+         cout << "| " << timetable[t][w][g] << " ";
        }
        cout << "|" << endl;
      }
@@ -81,18 +75,74 @@ void print_table() {
    }
 }
 
-void fill_timetable(string c) {
-  cout << c << endl;
+void fill_timetable(int row, int col, string class_name) {
+  // cout << "class_name = " << class_name << endl;
+  // cout << "class_name.length() = " << class_name.length() << endl;
+  for (int i = 0; i <= class_name.length()/15; i++) {
+    string grid_row;
+    if (class_name.length()>(i+1)*15) {
+      // cout << "range = " << i*13 << ", " << (i+1)*13-1 << endl;
+      grid_row = class_name.substr(i*15, 15);
+      // cout << grid_row.length() << endl;
+    }
+    else{
+      grid_row = class_name.substr(i*15, class_name.length()-i*15);
+      for (int s = 0; s < (i+1)*15-class_name.length(); s++)
+        grid_row += " ";
+    }
+
+    timetable[row][col][i] = grid_row;
+    // cout << "timetable[" << row << "][" << col << "][" << i << "] = " << endl;
+    // cout << grid_row << endl;
+  }
 }
+
+
+void parse_time(string c) {
+  int pos = c.find('-');
+  // time
+  string class_time = c.substr(0, pos);
+  c = c.substr(pos+1, c.length()-1);
+  pos = c.find('-');
+  string class_name = c.substr(pos+2, c.length()-1);
+  int row, col;
+  for (int i = 0; i < class_time.length(); i++) {
+
+    // weekday
+    if (class_time[i] >= '1' && class_time[i] <= '5') {
+      col = class_time[i] - '1';
+    }
+    // time
+    if (class_time[i] >= 'A' && class_time[i] <= 'K') {
+      row = class_time[i] - 'A';
+      fill_timetable(row, col, class_name);
+    }
+  }
+}
+
+void init_table() {
+  for (int r = 0; r < 11; r++) {
+    for (int c = 0; c < 5; c++) {
+      for (int i = 0; i < 4; i++) {
+        timetable[r][c][i] = ".              ";
+      }
+    }
+  }
+}
+
 
 int main(int argc, char const *argv[]) {
   std::vector<string> classes;
   fstream fin;
   fin.open(argv[1],ios::in);
+  init_table();
   while(fin.getline(line,sizeof(line),'\n')){
-      fill_timetable(line);
+      parse_time(line);
   }
-  // print_table();
+  // cout << "test" << endl;
+  // cout << "timetable[2][0][0] = " << timetable[2][0][0] << endl;
+  print_table();
+
 
 
   return 0;
