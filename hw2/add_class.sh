e50a78107a61a8ad2e2f36c26bd4182f73ed51d6
cysun0226 <j6256448@gmail.com>
2018-10-18 03:04:52 +0800

    clean comments, remove print table (only update cur_class.txt)

--- hw2/add_class.sh
+++ hw2/add_class.sh
@@ -16,22 +16,4 @@ usr_input=$(dialog --buildlist "Add a class" 30 100 20 "${class_items[@]}" --out
 
 cur_class=$(sed 's@\\@@g' <<< $usr_input)
 
-# first="I love Suzy and Mary"
-# second="Sara"
-# first=${first/Suzy/$second}
-
 eval 'for word in '$cur_class'; do echo $word; done' > ./data/cur_class.txt
-timetable=$(./print_table.out ./data/cur_class.txt)
-# ./print_table.out ./data/cur_class.txt > ./data/timetable.txt
-# echo "$timetable"
-
-dialog --no-collapse --title "Timetable" \
-           --help-button --extra-button --extra-label "EXTRA" \
-           --ok-label "Agree" --msgbox "$timetable" 50 100
-
-# for c in $(seq 1 ${#cur_class})
-# do
-#   if [ "${cur_class:c-1:1}"!='C' ]; then
-#     printf '%s' ${cur_class:c-1:1}
-#   fi
-# done
