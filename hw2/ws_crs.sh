991aacc597cc2381ca40a57eaffefce30ecaa63e
cysun0226 <j6256448@gmail.com>
2018-10-21 00:17:32 +0800

    add ws_crs.sh

---
+++ hw2/ws_crs.sh
@@ -0,0 +1,9 @@
+
+
+
+### main ----------------------
+
+dialog --title "Check Courses Data" \
+--defaultno --yesno \
+"Welcome to CRS.\n\nCurrent courses: \n * CS107-fall\n\nDownload new courses?"\
+ 20 50
