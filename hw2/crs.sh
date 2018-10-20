d2763e0325008083b35c78979a890d28783bd628
cysun0226 <j6256448@gmail.com>
2018-10-20 14:46:30 +0800

    add time conflict, fix added classes repeat

--- hw2/crs.sh
+++ hw2/crs.sh
@@ -10,7 +10,7 @@ fi
 # timetable.json
 
 # classes.txt
-#  - should record on/off
+# - generate from timetable.json
 
 ## ./data/cur_class.txt
 #  - change after "Add_class", default empty.
@@ -19,9 +19,10 @@ if [ ! -f "./data/cur_class.txt" ]; then
 fi
 ## display option
 show_classroom=0
-hide_col=0
+show_extra=0
+time_conflict=0
 opt1_str='Show Classroom'
-opt2_str='Hide Extra Column'
+opt2_str='Show Extra Column'
 
 ### functions ---------------
 handle_option()
@@ -36,16 +37,20 @@ handle_option()
          opt1_str='Show Class Name'
        fi
        ;;
-    op2) echo "op2"
-       break
+    op2) show_extra=$(expr 1 - $show_extra)
+       if [ $show_extra = 0 ];
+       then
+         opt2_str='Show extra column'
+       else
+         opt2_str='Hide extra column'
+       fi
        ;;
   esac
 }
 
 add_class()
 {
-  filename='classes.txt'
-  IFS=$'\n' read -d '' -r -a class < $filename
+  time_conflict=0
   filename='./data/cur_class.txt'
   IFS=$'\n' read -d '' -r -a added_class < $filename
   for i in ${!class[@]}
@@ -61,9 +66,30 @@ add_class()
   class_items+=( "${class[$i]}" "${class[$i]}" "$status" )
   done
   # buildlist
-  usr_input=$(dialog --buildlist "Add a class" 30 100 20 "${class_items[@]}" --output-fd 1)
+  usr_input=$(dialog --buildlist "Add a class" 30 100 20 \
+  "${class_items[@]}" --output-fd 1)
+  unset class_items
+  if [ "$usr_input" = "" ]; then
+    return
+  fi
   cur_class=$(sed 's@\\@@g' <<< $usr_input)
-  eval 'for word in '$cur_class'; do echo $word; done' > ./data/cur_class.txt
+  # eval 'for word in '$cur_class'; do echo $word; done' > ./data/cur_class.txt
+  eval 'for word in '$cur_class'; do echo $word; done' > ./data/temp.txt
+  add_result=$(./print_table.out ./data/temp.txt)
+  if [ "$add_result" != "conflict" ];
+  then
+    eval 'for word in '$cur_class'; do echo $word; done' > ./data/cur_class.txt
+  else
+    dialog --title "Warning" --msgbox "Time conflict!" 10 20
+    time_conflict=1
+  fi
+  rm ./data/temp.txt
+}
+
+init()
+{
+  filename='classes.txt'
+  IFS=$'\n' read -d '' -r -a class < $filename
 }
 
 
@@ -74,7 +100,7 @@ add_class()
 
 dialog --title "Check Courses Data" \
 --defaultno --yesno \
-"Welcome to CRS.\nCurrent courses: \n * CS107-fall\n\nDownload new courses?"\
+"Welcome to CRS.\n\nCurrent courses: \n * CS107-fall\n\nDownload new courses?"\
  20 50
 
 response=$?
@@ -84,22 +110,28 @@ case $response in
   255) echo "[ESC] key pressed.";;
 esac
 
+init
+
 # display timetable
 while [ $response != 2 ]; do
-  if [ $show_classroom = 0 ]
-    then
-      timetable=$(./print_table.out ./data/cur_class.txt)
-    else
-      timetable=$(./print_table.out ./data/cur_class.txt show_classroom)
-  fi
+  timetable=$(./print_table.out ./data/cur_class.txt $show_classroom $show_extra)
+  # if [ $show_classroom = 0 ]
+  #   then
+  #     timetable=$(./print_table.out ./data/cur_class.txt)
+  #   else
+  #     timetable=$(./print_table.out ./data/cur_class.txt show_classroom)
+  # fi
   dialog --no-collapse --title "Timetable" \
              --help-button --help-label "Exit" \
              --extra-button --extra-label "Option" \
-             --ok-label "Add Class" --msgbox "$timetable" 50 100
+             --ok-label "Add Class" --msgbox "$timetable" 50 130
 
   response=$?
   case $response in
     0) add_class
+       while [ $time_conflict = 1 ]; do
+         add_class
+       done
        ;;
     2) break
        ;;
