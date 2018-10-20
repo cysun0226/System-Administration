4f501322acf590300c0e64bc34cfc00b2ae63386
cysun0226 <j6256448@gmail.com>
2018-10-21 00:23:25 +0800

    update ws_crs.sh

--- hw2/ws_crs.sh
+++ hw2/ws_crs.sh
@@ -1,5 +1,9 @@
 
-
+init()
+{
+  filename='classes.txt'
+  IFS=$'\n' read -d '' -r -a class < $filename
+}
 
 ### main ----------------------
 
@@ -7,3 +11,12 @@ dialog --title "Check Courses Data" \
 --defaultno --yesno \
 "Welcome to CRS.\n\nCurrent courses: \n * CS107-fall\n\nDownload new courses?"\
  20 50
+
+ response=$?
+ case $response in
+   0) echo "Please input curl URL:";;
+   1) echo "Go on.";;
+   255) echo "[ESC] key pressed.";;
+ esac
+
+ init
