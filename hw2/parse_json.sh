6cd237282578a652804af3db7b38cba41f9065cb
cysun0226 <j6256448@gmail.com>
2018-10-21 13:51:18 +0800

    update parse_json.sh

--- hw2/parse_json.sh
+++ hw2/parse_json.sh
@@ -5,9 +5,9 @@ get_value()
   # semi_colon_count = 0;
   sc_cnt=0
 
-  while read -n1 c; do
-    echo "$c"
-  done < <(echo -n "$1")
+  # while read -n1 c; do
+  #   echo "$c"
+  # done < <(echo -n "$1")
 }
 
 parse_json()
@@ -29,12 +29,12 @@ prep_file='pre_classes.txt'
 # parse_json '"cos_ename":"Calculus(I)",'
 get_value '"cos_ename":"Calculus(I)",'
 
-# while read -n1 c; do
-#   pre_process+="$c"
-#   if [ "$c" = '{' ] || [ "$c" = '}' ] || [ "$c" = ',' ];
-#   then
-#     pre_process+='\n'
-#   fi
-# done < $raw_file
-#
-# echo "$pre_process" > "$prep_file"
+while read -n1 c; do
+  pre_process+="$c"
+  if [ "$c" = '{' ] || [ "$c" = '}' ] || [ "$c" = ',' ];
+  then
+    pre_process+='\n'
+  fi
+done < $raw_file
+
+echo "$pre_process" > "$prep_file"
