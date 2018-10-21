690bf1a154daa344411ddb63c28865d8e9ae6080
cysun0226 <j6256448@gmail.com>
2018-10-22 03:00:04 +0800

    add add_class

--- hw2/ws_crs.sh
+++ hw2/ws_crs.sh
@@ -6,15 +6,48 @@ time_conflict=0
 opt1_str='Show Classroom'
 opt2_str='Show Extra Column'
 
-init()
+handle_option()
 {
-  filename='classes.txt'
-  IFS=$'\n' read -d '' -r -a class < $filename
+  ipt=$1
+  case $ipt in
+    op1) show_classroom=$(expr 1 - $show_classroom)
+       if [ $show_classroom = 0 ];
+       then
+         opt1_str='Show Classroom'
+       else
+         opt1_str='Show Class Name'
+       fi
+       ;;
+    op2) show_extra=$(expr 1 - $show_extra)
+       if [ $show_extra = 0 ];
+       then
+         opt2_str='Show Extra Column'
+       else
+         opt2_str='Hide Extra Column'
+       fi
+       ;;
+  esac
+}
+
+generate_list_item()
+{
+  while read p; do
+    printf '"%s" ' "$p"
+    t=$(echo "$p" | sed 's/?/\ -\ /g')
+    printf '"%s" ' "$t"
+    printf '"%s" ' "off"
+  done < $1
 }
 
 add_class()
 {
-  echo "add_class"
+  usr_input=$(eval dialog --buildlist '"Add a class"' 30 100 20 "$(generate_list_item ./data/classes.txt)" --output-fd 1)
+  # cancel
+  if [ "$usr_input" = "" ]; then
+    return
+  fi
+  cur_class=$(echo "$usr_input" | sed 's@\\@@g')
+  eval 'for word in '$cur_class'; do echo $word; done' > ./data/cur_class.txt
 }
 
 
@@ -33,9 +66,7 @@ dialog --title "Check Courses Data" \
    255) echo "[ESC] key pressed.";;
  esac
 
- init
-
-timetable=$(./print_table.sh ./sample_sh.txt | sed 's/#/\ /g')
+timetable=$(./print_table.sh ./data/cur_class.txt | sed 's/#/\ /g')
 
 dialog --no-collapse --title "Timetable" \
            --help-button --help-label "Exit" \
@@ -54,6 +85,6 @@ case $response in
   3) echo "Option"
     opt=$(dialog --title "Option" --menu "Choose one" 12 35 5 \
     op1 "$opt1_str" op2 "$opt2_str" --output-fd 1)
-    # handle_option $opt
+    handle_option $opt
     ;;
 esac
