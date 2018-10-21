e24d15775f763622000629b3421749a44e1986ed
cysun0226 <j6256448@gmail.com>
2018-10-21 15:38:09 +0800

    finish parse_json.sh

--- hw2/parse_json.sh
+++ hw2/parse_json.sh
@@ -2,8 +2,13 @@
 
 get_value()
 {
-  # semi_colon_count = 0;
+  s_pos=$(awk -F: '{print length($1)+1}' <<< $1)
+  e_pos=$(echo -n $1 | wc -m)
+  s_pos=$(expr $s_pos + 2)
+  e_pos=$(expr $e_pos - 6)
   sc_cnt=0
+  n=$(echo $1 | cut -c $s_pos-$e_pos)
+  printf '%s' "$n"
 
   # while read -n1 c; do
   #   echo "$c"
@@ -12,14 +17,19 @@ get_value()
 
 parse_json()
 {
-  set 'cos_time' 'cos_ename'
-  shift 0
-  for t do
-    pos=$(echo $1 | grep -b -o "$t" | cut -d: -f1)
-    if [ "$pos" != "" ]; then
-      echo $pos
-    fi
-  done
+  # cos_time
+  pos=$(echo "$1" | grep -b -o 'cos_time' | cut -d: -f1)
+  if [ "$pos" != "" ]; then
+    get_value "$1"
+    printf '?'
+  fi
+
+  # cos_ename
+  pos=$(echo "$1" | grep -b -o 'cos_ename' | cut -d: -f1)
+  if [ "$pos" != "" ]; then
+    get_value "$1"
+    printf '\n'
+  fi
 }
 
 # main
@@ -27,15 +37,13 @@ raw_file='raw_timetable.json'
 prep_file='pre_classes.txt'
 # echo $raw_data
 
-parse_json '"cos_ename":"Calculus(I)",'
+# parse_json '"cos_ename":"Calculus(I)",'
 # get_value '"cos_ename":"Calculus(I)",'
 
+# insert new line into .json
 cat "$raw_file" | sed 's/'{'/{\'$'\n/g' | sed 's/'}'/}\'$'\n/g' | sed 's/','/,\'$'\n/g'  > "$prep_file"
 
-# while read -n1 c; do
-#   pre_process+="$c"
-#   if [ "$c" = '{' ] || [ "$c" = '}' ] || [ "$c" = ',' ];
-#   then
-#     pre_process+='\n'
-#   fi
-# done < $raw_file
+while read p; do
+    # echo $p
+    parse_json "$p"
+done < $prep_file
