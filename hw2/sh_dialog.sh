1694cec3a3423409dedd7f17b50b59b0321244ef
cysun0226 <j6256448@gmail.com>
2018-10-22 16:49:41 +0800

    update add_class buildlist

--- hw2/sh_dialog.sh
+++ hw2/sh_dialog.sh
@@ -2,32 +2,46 @@
 
 generate_list_item()
 {
+  # added_class
+  added_id=''
+  while read a; do
+    added_id="$added_id $(echo $a | cut -d'#' -f1)"
+  done < ./data/cur_class.txt
+
   while read p; do
-    # id=$(echo $p | cut -d'#' -f1)
+    id=$(echo $p | cut -d'#' -f1)
     time=$(echo $p | cut -d'#' -f2 | cut -d'?' -f1)
     name=$(echo $p | cut -d'?' -f2)
 
+    on_off='off'
+
+    for a in $added_id; do
+      if [ "$id" = "$a" ]; then
+        on_off='on'
+        break
+      fi
+    done
+
     printf '"%s" ' "$p"
     printf '"%s - %s" ' "$time" "$name"
-    printf '"%s" ' "off"
-
+    printf '"%s" ' "$on_off"
   done < $1
 }
 
-add_class()
-{
-  usr_input=$(eval dialog --buildlist '"Add a class"' 30 100 20 "$(generate_list_item ./data/n_class.txt)" --output-fd 1)
-  # cancel
-  if [ "$usr_input" = "" ]; then
-    return
-  fi
-  cur_class=$(sed 's@\\@@g' <<< $usr_input)
-  eval 'for word in '$cur_class'; do echo $word; done'
 
-}
+# add_class()
+# {
+#   usr_input=$(eval dialog --buildlist '"Add a class"' 30 100 20 "$(generate_list_item ./data/n_class.txt)" --output-fd 1)
+#   # cancel
+#   if [ "$usr_input" = "" ]; then
+#     return
+#   fi
+#   cur_class=$(sed 's@\\@@g' <<< $usr_input)
+#   eval 'for word in '$cur_class'; do echo $word; done' > ./data/ncur_class.txt
+# }
 
 
 # main
-# generate_list_item ./data/n_class.txt
-# eval dialog --buildlist '"Add a class"' 30 100 20 "$(generate_list_item ./data/n_class.txt)"
-add_class
+# generate_list_item ./data/classes.txt
+eval dialog --buildlist '"Add a class"' 30 100 20 "$(generate_list_item ./data/classes.txt)"
+# add_class
