a6b35ed6a9ec3c51a259c83871d6330ba3d27ec3
cysun0226 <j6256448@gmail.com>
2018-10-22 16:46:30 +0800

    update to new class format: id#time?name

--- hw2/parse_json.sh
+++ hw2/parse_json.sh
@@ -4,35 +4,43 @@ get_value()
 {
   v=$(echo $1 | cut -d':' -f2 | sed 's/.$//')
 
-  case $v in
-    ''|*[!0-9]*) ;;
-    *)
-      # is number
-      # check if redundant
-      rdd=0
-        for i in $exist_id; do
-          if [ "$v" = "$i" ]; then
-            rdd=1
-            break
-          fi
-        done
-      if [ "$rdd" = "0" ]; then
-         exist_id="$exist_id $v"
+  cnt=$(expr $cnt + 1 )
+  case $cnt in
+    1) #id
+        # check if redundant
+        rdd=0
+          for i in $exist_id; do
+            if [ "$v" = "$i" ]; then
+              rdd=1
+              break
+            fi
+          done
+        if [ "$rdd" = "0" ]; then
+           exist_id="$exist_id $v"
+           printf '%s#' "$v"
+        fi
+    ;;
+    2) # time
+      if [ "$rdd" = "1" ]; then
+        return
       fi
+      printf '%s?' "$v"
+    ;;
+    3) #name
+      cnt=0
+      if [ "$rdd" = "1" ]; then
+        return
+      fi
+      printf '%s\n' "$v"
     ;;
   esac
-
-  if [ "$rdd" = "1" ]; then
-    return
-  fi
-
-  printf '%s\n' "$v"
 }
 
 # main ======
 raw_file='raw_timetable.json'
 prep_file='pre_classes.txt'
 exist_id=''
+cnt=0
 
 # insert new line into .json
 cat "$1" | sed 's/'{'/{\'$'\n/g' | sed 's/'}'/}\'$'\n/g' | sed 's/'\",'/,\'$'\n/g' | awk '/cos_id/{print $0} /cos_time/{print $0} /cos_ename/{print $0}' | sed 's/"//g' > "$prep_file"
