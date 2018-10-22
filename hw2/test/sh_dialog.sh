320d9c196c14bdd33b966a7010d13ef1c9b36e59
cysun0226 <j6256448@gmail.com>
2018-10-23 02:58:03 +0800

    try new time conflict detection

--- hw2/test/sh_dialog.sh
+++ hw2/test/sh_dialog.sh
@@ -45,8 +45,65 @@ add_class()
   eval 'for word in '$cur_class'; do echo $word; done' > ./data/cur_class.txt
 }
 
+find_total_time()
+{
+  added_id=''
+  while read a; do
+    echo $a
+    x_time=$(echo $a | cut -d'#' -f2 | cut -d'?' -f1)
+    time_cnt=$(echo $x_time | grep -o '-' | wc -l)
+    time=''
+    for t in $(seq $time_cnt); do
+      time="$time$(echo $x_time | cut -d',' -f$t | cut -d'-' -f1)"
+    done
+    total_time="$total_time$time"
+  done < ./data/cur_class.txt
+}
+
+check_conflict()
+{
+  # $1 = total_time
+  Mon_t=''
+  Tue_t=''
+  Wed_t=''
+  Thu_t=''
+  Fri_t=''
+  Sat_t=''
+  Sun_t=''
+
+  for i in $(seq ${#total_time}); do
+    c=$(echo "$total_time" | cut -c $i-$i)
+    case $c in
+      1) wd='Mon_t'
+         echo $Mon_t | grep -o $c
+      ;;
+      2) wd='Tue_t'
+      ;;
+      3) wd='Wed_t'
+      ;;
+      4) wd='Thu_t'
+      ;;
+      5) wd='Fri_t'
+      ;;
+      6) wd='Sat_t'
+      ;;
+      7) wd='Sun_t'
+      ;;
+      *) tmp=$(eval echo "\$$wd")
+         eval $wd="$tmp$c"
+      ;;
+    esac
+  done
+
+  echo "$Mon_t"
+}
 
 # main
 # generate_list_item ./data/classes.txt
 # eval dialog --buildlist '"Add a class"' 30 100 20 "$(generate_list_item ./data/classes.txt)"
-add_class
+total_time=''
+conflict=0
+find_total_time
+echo "$total_time"
+check_conflict
+# add_class
