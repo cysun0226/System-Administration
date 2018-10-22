13b1183cc4f73743f9077250ad838d7c949f7cad
cysun0226 <j6256448@gmail.com>
2018-10-22 15:59:06 +0800

    finish new add_class

--- hw2/sh_dialog.sh
+++ hw2/sh_dialog.sh
@@ -2,13 +2,32 @@
 
 generate_list_item()
 {
-  filename='./data/cur_class.txt'
   while read p; do
+    # id=$(echo $p | cut -d'#' -f1)
+    time=$(echo $p | cut -d'#' -f2 | cut -d'?' -f1)
+    name=$(echo $p | cut -d'?' -f2)
+
     printf '"%s" ' "$p"
-    printf '"%s" ' "$p"
+    printf '"%s - %s" ' "$time" "$name"
     printf '"%s" ' "off"
-  done < $filename
+
+  done < $1
 }
 
+add_class()
+{
+  usr_input=$(eval dialog --buildlist '"Add a class"' 30 100 20 "$(generate_list_item ./data/n_class.txt)" --output-fd 1)
+  # cancel
+  if [ "$usr_input" = "" ]; then
+    return
+  fi
+  cur_class=$(sed 's@\\@@g' <<< $usr_input)
+  eval 'for word in '$cur_class'; do echo $word; done'
+
+}
+
+
 # main
-eval dialog --buildlist '"Add a class"' 30 100 20 "$(generate_list_item)"
+# generate_list_item ./data/n_class.txt
+# eval dialog --buildlist '"Add a class"' 30 100 20 "$(generate_list_item ./data/n_class.txt)"
+add_class
